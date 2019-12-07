f6419bbca841754db12a779b7980acac38e16dfb
tx-10 <tlee0027@student.monash.edu>
2019-12-07 15:57:54 +0800

    minor change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/js/dist/dropdown'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
