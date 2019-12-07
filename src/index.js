e9ebc0692035fca00daa3dea728b2dfde625d0b7
tx-10 <tlee0027@student.monash.edu>
2019-12-07 15:33:09 +0800

    added router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import 'bootstrap/js/dist/dropdown'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import BrowserRouter from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
